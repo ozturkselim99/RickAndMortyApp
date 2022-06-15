803cfa012f7a7cda9d3ef6002ac7afa2141e9759
ozturkselim99 <assessment-118@external.com.tr>
2022-06-15 14:08:21 +0300

    create folders structure for android clean architecture

--- app/src/main/java/com/selimozturk/rickandmortyapp/view/MainActivity.kt
+++ app/src/main/java/com/selimozturk/rickandmortyapp/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.selimozturk.rickandmortyapp
+package com.selimozturk.rickandmortyapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.selimozturk.rickandmortyapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
